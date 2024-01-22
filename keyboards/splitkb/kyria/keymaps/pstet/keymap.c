33ab7eaa3aaf9e968fd9ddfa3201b50bf55ff90f
pstet <123316024+pcs03@users.noreply.github.com>
2024-01-22 17:52:10 +0100

    Change mapping for thumb keys of Kyria

--- keyboards/splitkb/kyria/keymaps/pstet/keymap.c
+++ keyboards/splitkb/kyria/keymaps/pstet/keymap.c
@@ -66,8 +66,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_COLEMAK_DH] = LAYOUT(
         KC_TAB  , KC_Q ,  KC_W   ,  KC_F  ,   KC_P ,   KC_B ,                                        KC_J,   KC_L ,  KC_U ,   KC_Y ,KC_SCLN, KC_ESC,
         KC_LCTL , KC_A ,  KC_R   ,  KC_S  ,   KC_T ,   KC_G ,                                        KC_M,   KC_N ,  KC_E ,   KC_I ,  KC_O , KC_QUOT,
-        KC_LSFT , KC_Z ,  KC_X   ,  KC_C  ,   KC_D ,   KC_V , KC_LBRC,KC_CAPS,     SYM2   , KC_RBRC, KC_K,   KC_H ,KC_COMM, KC_DOT ,KC_SLSH, KC_RSFT,
-        KC_LGUI, KC_LALT, KC_SPC , SYM1   , FKEYS ,     NAV    , SYM2   , KC_ENT , KC_BSPC, KC_RALT
+        KC_LSFT , KC_Z ,  KC_X   ,  KC_C  ,   KC_D ,   KC_V , KC_CAPS, ADJUST,     ADJUST , KC_RBRC, KC_K,   KC_H ,KC_COMM, KC_DOT ,KC_SLSH, KC_RSFT,
+                                   KC_LGUI, KC_LALT, KC_SPC , SYM1   , FKEYS ,     NAV    , SYM2   , KC_ENT , KC_BSPC, KC_RALT
     ),
 
     /*
@@ -87,8 +87,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT(
         KC_TAB  , KC_Q ,  KC_W   ,  KC_E  ,   KC_R ,   KC_T ,                                        KC_Y,   KC_U ,  KC_I ,   KC_O ,  KC_P , KC_BSPC,
         CTL_ESC , KC_A ,  KC_S   ,  KC_D  ,   KC_F ,   KC_G ,                                        KC_H,   KC_J ,  KC_K ,   KC_L ,KC_SCLN, KC_QUOT,
-        KC_LSFT , KC_Z ,  KC_X   ,  KC_C  ,   KC_V ,   KC_B , KC_CAPS, ADJUST,     FKEYS  , KC_RBRC, KC_N,   KC_M ,KC_COMM, KC_DOT ,KC_SLSH, KC_RSFT,
-        KC_LGUI, KC_LALT, KC_SPC , SYM1   , FKEYS ,     NAV    , SYM2   , KC_ENT , KC_BSPC, KC_RALT
+        KC_LSFT , KC_Z ,  KC_X   ,  KC_C  ,   KC_V ,   KC_B , KC_CAPS, ADJUST,     ADJUST , KC_RBRC, KC_N,   KC_M ,KC_COMM, KC_DOT ,KC_SLSH, KC_RSFT,
+                                   KC_LGUI, KC_LALT, KC_SPC , SYM1   , FKEYS ,     NAV    , SYM2   , KC_ENT , KC_BSPC, KC_RALT
     ),
 
     /*
@@ -108,8 +108,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_DVORAK] = LAYOUT(
         KC_TAB  ,KC_QUOTE,KC_COMM,  KC_DOT,   KC_P ,   KC_Y ,                                        KC_F,   KC_G ,  KC_C ,   KC_R ,  KC_L , KC_BSPC,
         CTL_ESC , KC_A ,  KC_O   ,  KC_E  ,   KC_U ,   KC_I ,                                        KC_D,   KC_H ,  KC_T ,   KC_N ,  KC_S , CTL_MINS,
-        KC_LSFT ,KC_SCLN, KC_Q   ,  KC_J  ,   KC_K ,   KC_X , KC_LBRC,KC_CAPS,     FKEYS  , KC_RBRC, KC_B,   KC_M ,  KC_W ,   KC_V ,  KC_Z , KC_RSFT,
-        KC_LGUI, KC_LALT, KC_SPC , SYM1   , FKEYS ,     NAV    , SYM2   , KC_ENT , KC_BSPC, KC_RALT
+        KC_LSFT ,KC_SCLN, KC_Q   ,  KC_J  ,   KC_K ,   KC_X , KC_LBRC, ADJUST,     ADJUST , KC_RBRC, KC_B,   KC_M ,  KC_W ,   KC_V ,  KC_Z , KC_RSFT,
+                                   KC_LGUI, KC_LALT, KC_SPC , SYM1   , FKEYS ,     NAV    , SYM2   , KC_ENT , KC_BSPC, KC_RALT
     ),
 
 /*
